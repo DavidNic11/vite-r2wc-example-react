638d5d0ec7b5b22c04d8487de425b0e3aaf3bec4
David Nicholas <Dnicholas@bitovi.com>
2022-12-12 17:23:17 -0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "vite-r2wc-example-react",
+  base: "/vite-r2wc-example-react",
   plugins: [react()],
 });
